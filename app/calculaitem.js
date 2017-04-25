1f9d8d800e3055bd9abcdedefbb496a9eff7b452
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-04-25 20:55:31 -0300

    primeiros arquivos

---
+++ app/calculaitem.js
@@ -0,0 +1,6 @@
+{ produtos:"laranja, morango, bergamota, caju" {
+{id: 1, nome: "laranja", valorUnitario: 3};
+{id: 2, nome: "morango", valorUnitario: 5};
+{id: 3, nome: "bergamota", valorUnitario: 2};
+{id: 4, nome: "caju", valorUnitario: 7};
+}}
