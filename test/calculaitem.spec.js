d289e2a9cb0530a3466bd82b27b1e7218200856e
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-04-25 21:29:55 -0300

    primeiro teste

---
+++ test/calculaitem.spec.js
@@ -0,0 +1,19 @@
+const Lab = require('lab');
+const lab = export.lab = Lab.script();
+
+const { expect } = require('code');
+
+const { calculaItem } = require('../app/contaItem');
+
+lab.experiment('quando não informa id',() => {
+  lab.test('valor = unxepct', (done) => {
+    let ci = calculaitem();
+    expect(ci.resultado()).to.equal({resultado: []});
+    done();
+  });
+});
+
+lab.experiment('quando informa id',()
+if(!id = 1)
+  let quantidade1 => 1
+  quantidade * 3 == valortotal1 ;
