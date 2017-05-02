c1e2a4a1db4f53cb8aa11c20a965f097289a4cd8
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-05-02 20:01:52 -0300

    primeiro teste bajeitado de acordo com a ajuda do professor

--- test/calculaitem.spec.js
+++ test/calculaitem.spec.js
@@ -3,7 +3,14 @@ const lab = export.lab = Lab.script();
 
 const { expect } = require('code');
 
-const { calculaItem } = require('../app/contaItem');
+const { calculaItem } = require('../app/calculaTotalItem');
+
+lab.experiment{"verifica calculo", () =>{
+  lab.test("quando o produto existe"), (done) => {
+    expect(calculaTotalItem(1,3)).to.equal({ produtos: 'laranja', valor: 9})
+      done();
+    }
+  }
 
 lab.experiment('quando não informa id',() => {
   lab.test('valor = unxepct', (done) => {
