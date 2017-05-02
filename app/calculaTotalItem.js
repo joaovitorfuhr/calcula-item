75870027eb9da05b37cc4e02fde06065aa5577d5
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-05-02 20:59:51 -0300

    ajeitando os códigos

--- app/calculaTotalItem.js
+++ app/calculaTotalItem.js
@@ -1,15 +1,11 @@
 const calculaTotalItem = (codigo, quantidade) => {
   let produtos = [
-    { produtos:'laranja, morango, bergamota, caju'}
-    {id: 1, nome: 'laranja', valorUnitario: 3};
-    {id: 2, nome: 'morango', valorUnitario: 5};
-    {id: 3, nome: 'bergamota', valorUnitario: 2};
-    {id: 4, nome: 'caju', valorUnitario: 7};
-    ]};
+    {id: 1, nome: 'laranja', valorUnitario: 3},
+    {id: 2, nome: 'morango', valorUnitario: 5},
+    {id: 3, nome: 'bergamota', valorUnitario: 2},
+    {id: 4, nome: 'caju', valorUnitario: 7},
+    ];
 
-    return{
-      resultado: () => {
-        return {codigo, quantidade};
-      }
+    return {produto: '' valor: 0}
 
 module.export = {calculaTotalItem}
--- test/calculaTotalItem.spec.js
+++ test/calculaTotalItem.spec.js
@@ -3,11 +3,11 @@ const lab = export.lab = Lab.script();
 
 const { expect } = require('code');
 
-const { calculaItem } = require('../app/calculaTotalItem');
+const { calculaTotalItem } = require('../app/calculaTotalItem');
 
 lab.experiment{"verifica calculo", () =>{
   lab.test("quando o produto existe"), (done) => {
-    expect(calculaTotalItem(1,3)).to.equal({ produtos: 'laranja', valor: 9})
-      done();
-    }
-  };
+    expect(calculaTotalItem(1,3)).to.equal({ produto: 'laranja', valor: 9});
+    done();
+  }
+};
