4f7a3bd89a98ecb28fe8b2656160979bfabc2f9f
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-05-02 20:37:26 -0300

    concerto do nome

--- test/calculaTotalItem.spec.js
+++ test/calculaTotalItem.spec.js
@@ -10,19 +10,4 @@ lab.experiment{"verifica calculo", () =>{
     expect(calculaTotalItem(1,3)).to.equal({ produtos: 'laranja', valor: 9})
       done();
     }
-  }
-
-lab.experiment('quando não informa id',() => {
-  lab.test('valor = unxepct', (done) => {
-    let ci = calculaitem();
-    expect(ci.resultado()).to.equal({resultado: []});
-    done();
-  });
-});
-
-lab.experiment('quando somar todos os id', () =>{
-  lab.test('id1 ++ id2 ++ id3 ++ id4 = r.resultado')
-  expect(r.resulatado => 4)
-
-
-)}
+  };
