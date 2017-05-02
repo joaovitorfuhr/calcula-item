61dffc17a90f2be12837c1dca6a60ac46e7e3077
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-05-02 20:33:20 -0300

    ajeitando tabela

--- app/calculaTotalItem.js
+++ app/calculaTotalItem.js
@@ -1,17 +1,15 @@
-{ produtos:'laranja, morango, bergamota, caju' {
-{id: 1, nome: 'laranja', valorUnitario: 3};
-{id: 2, nome: 'morango', valorUnitario: 5};
-{id: 3, nome: 'bergamota', valorUnitario: 2};
-{id: 4, nome: 'caju', valorUnitario: 7};
-}};
+const calculaTotalItem = (codigo, quantidade) => {
+  let produtos = [
+    { produtos:'laranja, morango, bergamota, caju'}
+    {id: 1, nome: 'laranja', valorUnitario: 3};
+    {id: 2, nome: 'morango', valorUnitario: 5};
+    {id: 3, nome: 'bergamota', valorUnitario: 2};
+    {id: 4, nome: 'caju', valorUnitario: 7};
+    ]};
 
-const { calculaItem } = require('./calculaItem');
-const calculaItem = ( id = {}) =>{}
+    return{
+      resultado: () => {
+        return {codigo, quantidade};
+      }
 
-id = [];
-opcoes;
-
-return {
-  resultado: () => {
-    return { resultado: id } ;
-}
+module.export = {calculaTotalItem}
