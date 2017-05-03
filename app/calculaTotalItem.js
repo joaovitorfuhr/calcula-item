391ae9e8dbbcf7c18c21a717778b9010f94e14df
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-05-02 21:32:11 -0300

    programa quase pronto

--- app/calculaTotalItem.js
+++ app/calculaTotalItem.js
@@ -5,7 +5,8 @@ const calculaTotalItem = (codigo, quantidade) => {
     {id: 3, nome: 'bergamota', valorUnitario: 2},
     {id: 4, nome: 'caju', valorUnitario: 7},
     ];
+  let valor = 0;
+  return {produto: 'laranja', valor: 9};
+};
 
-    return {produto: '' valor: 0}
-
-module.export = {calculaTotalItem}
+module.exports = { calculaTotalItem }
