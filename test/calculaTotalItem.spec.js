cecbbb140393b616860ce6bbe59855e2d14203e9
joaovitorfuhr <joaovf.jvf.12@gmail.com>
2017-05-02 21:31:25 -0300

    teste funcionando

--- test/calculaTotalItem.spec.js
+++ test/calculaTotalItem.spec.js
@@ -1,13 +1,13 @@
 const Lab = require('lab');
-const lab = export.lab = Lab.script();
+const lab = exports.lab = Lab.script();
 
 const { expect } = require('code');
 
 const { calculaTotalItem } = require('../app/calculaTotalItem');
 
-lab.experiment{"verifica calculo", () =>{
-  lab.test("quando o produto existe"), (done) => {
+lab.experiment("verifica calculo", () => {
+  lab.test("quando o produto existe", (done) => {
     expect(calculaTotalItem(1,3)).to.equal({ produto: 'laranja', valor: 9});
     done();
-  }
-};
+  })
+});
